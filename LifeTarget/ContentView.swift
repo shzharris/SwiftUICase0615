ac90f1f9d0fe491eb157507c4c05869a94ecf119
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-15 17:01:23 +0800

    Initial Commit

---
+++ LifeTarget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeTarget
+//
+//  Created by HarrisShao on 2025/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifeTarget/LifeTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTargetApp.swift
+//  LifeTarget
+//
+//  Created by HarrisShao on 2025/6/15.
+//
+
+import SwiftUI
+
+@main
+struct LifeTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
